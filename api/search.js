881416640abd3909cf04090ea2c0e9be85f7c934
Korchev <Korchev94@gmail.com>
2021-11-26 08:13:24 +0000

    attempt to fix bug for vercel

---
+++ api/search.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const cheerio = require("cheerio")
+const puppeteer = require("puppeteer")
+const router = express.Router()
+
+const search = async (searchTerm) => {
+  const url = `https://www.parfumo.net/s_perfumes.php?lt=1&filter=${searchTerm}`
+
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const content = await page.goto(url).then(async () => page.content())
+    const $ = cheerio.load(content)
+    const perfumeGrid = $(".pgrid").find(".col")
+    const objects = []
+    perfumeGrid.each((i, el) => {
+      const name = $(el).find(".name > a").text()
+      const brand = $(el).find(".name > .brand > a").text()
+      const imageUrl = $(el).find("img").attr("data-src")
+      objects.push({
+        name: `${brand} - ${name}`,
+        imageUrl,
+      })
+    })
+    await browser.close()
+
+    return objects
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+router.get("/:searchTerm", async (req, res) => {
+  try {
+    const term = req.params.searchTerm
+    const data = await search(term)
+    res.json(data)
+  } catch (error) {
+    res.status(500)
+  }
+})
+
+module.exports = router
